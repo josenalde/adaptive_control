de9d883b0c2306403c117bd306174ddcb68f1c52
josenaldeOliveira <josenalde@gmail.com>
2023-06-20 18:04:07 -0300

    Add files via upload

--- scripts/gradient_integral_1.m
+++ scripts/gradient_integral_1.m
@@ -11,28 +11,25 @@ a1 = 1;
 num = [0 kp];
 den = [1 a1];
 ftx = tf(num,den);
-km = 2;
-am1 = 2;
 
 dt = 0.1;
-tMax = 500;
+tMax = 100;
 
 N = tMax/dt;
 T = 0:dt:N;
 % input
 %u = ones(1,N/dt+1);
-%r = ones(1,N/dt+1);
-r = 1 + 0.3*sin(T) + 0.3*cos(2*T);
-%r = 1 + (0.5*sin(T) + 1.2*cos(2*T));
+
+u = 0.5*sin(T) + 1.2*cos(2*T);
 %u = 0.1*cos((pi/7)*T) + 0.05*sin((pi/5)*T);
-[Y,T] = lsim(ftx,r,T);
+[Y,T] = lsim(ftx,u,T);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% gradient method J instantaneous
+% gradient method J integral
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 Erro(1)     = 0;
-theta(:,1)  = [-1.0;3];
+theta(:,1)  = [0;0];
 gamma_1 = 0.5;
 gamma_2 = 0.1;
 Gamma = [gamma_1 0;
@@ -46,65 +43,22 @@ z = lsim(f_z, Y, T);
 f_phi_1 = tf([0 1],[1 2]);
 f_phi_2 = tf([0 1], [1 2]);
 
-%phi_1 = lsim(f_phi_1, r, T);
+phi_1 = lsim(f_phi_1, u, T);
 phi_2 = lsim(f_phi_2, -Y, T);
 
-t = 0;
-
-
-u = zeros(1, tMax/dt + 2);
-tplot = zeros(1, tMax/dt + 2);
-y = zeros(1, tMax/dt + 2);
-ym = zeros(1, tMax/dt + 2);
 k = 2;
-e = zeros(1, tMax/dt + 2);
-theta_1 = zeros(1, tMax/dt + 2);
-theta_2 = zeros(1, tMax/dt + 2);
-phi_1(1) = 0;
 
+theta_1_star = 0.5;
+theta_2_star = 1;
 
-theta_1_star = ((a1 - am1) / kp) *ones(1, tMax/dt + 2);
-theta_2_star = (km/kp)*ones(1, tMax/dt + 2);
+theta_1_star = theta_1_star * ones(1, length(T));
+theta_2_star = theta_2_star * ones(1, length(T));
 
-beta = 0.1; %para funÃ§Ã£o de custo integral
+beta = 0.1; %para função de custo integral
 
 q(:,1) = [0;0];
 R(:,:,1) = [0 0; 0 0];
 
-
-while (t < tMax)
-
-   y(k) = y(k-1) + dt*(-a1*y(k-1) + kp*u(k-1));
-   r(k) = 1;
-   %r(k) = 0.1*cos((pi/7)*t) + 0.05*sin((pi/5)*t);
-   %r(k) = 1 + 0.3*sin(t) + 0.3*cos(2*t);
-   ym(k) = ym(k-1) + dt*(-am1*ym(k-1) + km*r(k));
-   %u(k) = theta_1_star*y(k) + theta_2_star*r(k);
-   e(k) = y(k) - ym(k);
-   % leis adaptativas Lyapunov
-   theta_1(k) = theta_1(k-1) + dt*(-gamma_1 * e(k) * y(k));
-   theta_2(k) = theta_2(k-1) + dt*(-gamma_2 * e(k) * r(k));
-   % leis adaptativas gradiente integral
-   phi_1(k) = phi_1(k-1) + dt*(-2*phi_1(k-1) + u(k-1));
-   Phi(:,k)   = [phi_1(k); phi_2(k)];
-   m2          = 1 + alpha*Phi(:,k)'*Phi(:,k);
-   q(:,k) = q(:,k-1) + dt*(-beta * q(:,k-1) - ((z(k) * Phi(:,k)) / m2));
-   R(:,:,k) = R(:,:,k-1) + dt*(-beta * R(:,:,k-1) + ((Phi(:,k)*Phi(:,k)')/m2));
-   theta(:,k) = theta(:,k-1) + dt*(-Gamma*(R(:,:,k)*theta(:,k-1) + q(:,k)));
-   %u(k) = theta_1(k)*y(k) + theta_2(k)*r(k);
-   u(k) = theta(1,k)*y(k) + theta(2,k)*r(k);
-   t = t + dt;
-   tplot(k) = t;
-   k = k + 1;
-endwhile
-
-plot(tplot(1:length(tplot)-2), y(1:length(tplot)-2), 'k', 'LineWidth', 2, tplot(1:length(tplot)-2), ym(1:length(tplot)-2), 'b', 'LineWidth', 2);
-figure;
-%plot(tplot(1:length(tplot)-2), theta_1(1:length(tplot)-2), 'k', 'LineWidth', 2, tplot(1:length(tplot)-2), theta_1_star(1:length(tplot)-2), 'b--', 'LineWidth', 2);
-plot(tplot, theta(1,:), 'k', 'LineWidth', 2, tplot(1:length(tplot)-2), theta_1_star(1:length(tplot)-2), 'b--', 'LineWidth', 2);
-figure;
-%plot(tplot(1:length(tplot)-2), theta_2(1:length(tplot)-2), 'k', 'LineWidth', 2, tplot(1:length(tplot)-2), theta_2_star(1:length(tplot)-2), 'b--', 'LineWidth', 2);
-plot(tplot, theta(2,:), 'k', 'LineWidth', 2, tplot(1:length(tplot)-2), theta_2_star(1:length(tplot)-2), 'b--', 'LineWidth', 2);
 ##for k = 1:N/dt
 ##    omega(:,k) = [Y(k); r(k)];
 ##    Phi(:,k)   = [phi_1(k); phi_2(k)];
@@ -112,18 +66,17 @@ plot(tplot, theta(2,:), 'k', 'LineWidth', 2, tplot(1:length(tplot)-2), theta_2_s
 ##    erro(k+1)   = (z(k) - theta(:,k)'*Phi(:,k))/m2;
 ##    theta(:,k+1) = theta(:,k) + Gamma*dt*erro(k+1)*Phi(:,k);
 ##end
-##
-##subplot(2,1,1);
-##b0_plot = b0*ones(1, length(theta(1,:)));
-##plot(T, b0_plot, 'k-', 'LineWidth', 2, T, theta(1,:), 'k--', 'LineWidth', 2);
-##title('Gradient method - integral cost function');
-##set(gca, "fontsize", 12);
-##axis([0, length(T)*dt, 0, 1]);
-##subplot(2,1,2);
-##a1_plot = a1*ones(1, length(theta(2,:)));
-##plot(T, a1_plot, 'k-', 'LineWidth', 2, T, theta(2,:), 'k--', 'LineWidth', 2);
-##set(gca, "fontsize", 12);
-##axis([0, length(T)*dt, 0, 1.5]);
-##xlabel('Samples');
+for k = 1:N/dt
+   Phi(:,k)   = [phi_1(k); phi_2(k)];
+   m2          = 1 + alpha*Phi(:,k)'*Phi(:,k);
+   q(:,k+1) = q(:,k) + dt*(-beta * q(:,k) - ((z(k) * Phi(:,k)) / m2));
+   R(:,:,k+1) = R(:,:,k) + dt*(-beta * R(:,:,k) + ((Phi(:,k)*Phi(:,k)')/m2));
+   theta(:,k+1) = theta(:,k) + dt*(-Gamma*(R(:,:,k+1)*theta(:,k) + q(:,k+1)));
+endfor
+
+plot(T, theta(1,:), 'k', 'LineWidth', 2, T, theta_1_star, 'b--', 'LineWidth', 2);
+figure;
+plot(T, theta(2,:), 'k', 'LineWidth', 2, T, theta_2_star, 'b--', 'LineWidth', 2);
+
 clear;
 
